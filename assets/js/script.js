41fd8a0071caf8f2f42e49a4756533b18c1bd4e4
Greenie115 <dggreen545@gmail.com>
2023-11-04 12:18:36 +0000

    Get element variables added to script.js 2-11

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
 //list of variables targeting the right element on the DOM
-var 
+var timeCount = document.getElementById("time");
+var startButton = document.getElementById("start");
+var startScreen = document.getElementById("start-screen");
+var questionsMain = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title");
+var choices = document.getElementById("choices");
+var endScreen = document.getElementById("end-screen");
+var finalScore = document.getElementById("final-score");
+var hsInitials = document.getElementById("initials");
+var hsButton = document.getElementById("submit");
+
