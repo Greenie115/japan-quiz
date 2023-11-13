01ccbb33f5ba5d23f0a60a6bbb92f1f7ebce64ea
Greenie115 <dggreen545@gmail.com>
2023-11-13 19:31:27 +0000

    working forEach loop updating the answers on each click

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,72 +1,31 @@
-//array of objects containing the questions
-// var quizQuestions = [
-    
-//     questions = {
-//         questionOne: "What is the capital of Japan",
-//         questionTwo: "Where is...",
-//         questionThree: "How many...",
-//         questionFour: "Is there...",
-//         questionFive: "Why does..."
-//     },
 
-//   firstChoices =  {
-//         option1: "answer 1",
-//         answer: "This is the answer",
-//         option2: "answer 3",
-//         option3: "answer 4"
-//     },
-//     secondChoices = {
-//         answer: "Tokyo",
-//         option2: "answer 2",
-//         option3: "answer 3",
-//         option4: "answer 4"
-//     },
-//     thirdChoices = {
-//         answer: "Tokyo",
-//         option2: "answer 2",
-//         option3: "answer 3",
-//         option4: "answer 4"
-//     },
-//     fourthChoices = {
-//         answer: "Tokyo",
-//         option2: "answer 2",
-//         option3: "answer 3",
-//         option4: "answer 4"
-//     },
-//     fifthChoices = {
-//         answer: "Tokyo",
-//         option2: "answer 2",
-//         option3: "answer 3",
-//         option4: "answer 4"
-//     }
-
-// ];
-
-
-var quizQuestions = {
-    question1: {
+var quizQuestions = [
+   {
+        question: "This is the first question",
         choices: ["Here", "there", "nowhere", "another choice"],
         answer: "Here"
     },
 
-    question2: {
-        choices: ["choice 2", "there", "nowhere"],
+    {   
+        question: "Here os the second question",
+        choices: ["choice 2", "there", "nowhere", 'another choice'],
         answer: "Here"
     },
 
-    question3: {
+    {
+        question: "The third question",
         choices: ["Here", "choice 3", "nowhere"],
         answer: "Here"
     },
-
-    question4: {
-        choices: ["Here", "choice 4", "nowhere"],
+    {
+        question: "The third question",
+        choices: ["Here", "choice 3", "nowhere"],
         answer: "Here"
     },
-
-    question5: {
-        choices: ["Here", "there", "nowhere"],
+    {
+        question: "The third question",
+        choices: ["Here", "choice 3", "nowhere"],
         answer: "Here"
-    }}
+    }
+]
 
-    var questionsArray = ["question 1", "question 2", "question 3", "question 4", "question 5" ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,52 +9,85 @@ var endScreen = document.getElementById("end-screen");
 var finalScore = document.getElementById("final-score");
 var hsInitials = document.getElementById("initials");
 var hsButton = document.getElementById("submit");
+var answerPText = document.getElementById('correct');
 //---------- end ----------
 
-var increment = 0
-var choicesLength = quizQuestions.question1.choices.length
+var questionIndex = 0
+// var choicesLength = quizQuestions.question1.choices.length
+
+
 
 // var changeQuestion = questionTitle.textContent = questionTitleValue[increment]
 
 //timer variable to hold starting time
-var timer = 5;
+var timer = 60;
 //--- end ---
+var currentQuestion = quizQuestions[questionIndex]
 
-function loadQuestions(){
-  for (var i = 0; i < choicesLength; i++){
-      var liBtn = document.createElement("button")
-      choicesLi.append(liBtn)
-      liBtn.classList.add("choicesLi")
-      liBtn.textContent = quizQuestions[i].choices
-    }
-};
+function loadQuestions() {
+  choicesLi.innerHTML = ''
+  questionTitle.textContent = currentQuestion.question
+  currentQuestion.choices.forEach(function (choice) {
+    var liBtn = document.createElement("button")
+    liBtn.textContent = choice
+    liBtn.classList.add("choicesLi")
+    liBtn.onclick = answerClick
+    choicesLi.append(liBtn)
+
+  }
+  )
+
+  // for (var i = 0; i < choicesLength; i++) {
+
+
+}
+
+function answerClick(){
+  var userChoice = this.innerHTML
+  var correctAnswer = currentQuestion.answer
+  answerPText.setAttribute('class', 'show')
+
+  if(userChoice === correctAnswer){
+    answerPText.textContent = "===== Correct ====="
+  } else {
+    answerPText.textContent = "===== Wrong ====="
+    timer -= 10 
+  }
+    setTimeout(function(){
+      answerPText.setAttribute('class', 'hide')
+      questionIndex ++
+      currentQuestion = quizQuestions[questionIndex]
+      loadQuestions()
+    }, 1000)
+  // console.log(userChoice, correctAnswer)
+}
+// };
 
 // event listener on the start button
-startButton.addEventListener("click", function(){
-    startScreen.setAttribute("class", "hide")
-    questionsMain.setAttribute("class", "show")
-    choicesLi.setAttribute("class", "show")
-    choicesLi.addEventListener("click", function(){
-    increment ++
-    questionTitle.textContent = questionsArray[increment]
-    choicesLi.textContent = quizQuestions.choices[increment]
-    })
+startButton.addEventListener("click", function () {
+  startScreen.setAttribute("class", "hide")
+  questionsMain.setAttribute("class", "show")
+  choicesLi.setAttribute("class", "show")
+
   //function counting down the timer once button is clicked 
-  var timerInterval = setInterval(function(){
+  var timerInterval = setInterval(function () {
     timer--;
     timeCount.textContent = timer
-  //error handler to stop count at 0  
-  if(timer <= 0 || increment > 4){
-    clearInterval(timerInterval)
-    endScreen.setAttribute("class", "show")
-    choicesLi.setAttribute("class", "hide")
-    questionTitle.setAttribute("class", "hide")
-    finalScore.textContent = timer
-  }
+    //error handler to stop count at 0  
+    if (timer <= 0 || questionIndex > 4) {
+      clearInterval(timerInterval)
+      endScreen.setAttribute("class", "show")
+      choicesLi.setAttribute("class", "hide")
+      questionTitle.setAttribute("class", "hide")
+      finalScore.textContent = timer
+    }
   }, 1000)
   loadQuestions();
-  
-    
+
+
 }) //----- end -----
 
 //loop for creating each answers and questions button
+
+
+console.log()
